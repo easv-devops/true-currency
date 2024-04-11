d250f46708aa65f76c9ad1b80731e1f96f8485f8
micdra01 <114472076+micdra01@users.noreply.github.com>
2024-04-11 11:56:14 +0200

    >frontend > redirect/refresh fix attempt

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import {HomePage} from "./home/home.page";
+import {AppComponent} from "./app.component";
+import {HomePageModule} from "./home/home.module";
 
 const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePage
+  },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   }
 ];
 
