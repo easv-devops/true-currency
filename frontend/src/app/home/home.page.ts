e882ddd196d6da0e1d5deba8c0d4585212512dcd
micdra01 <114472076+micdra01@users.noreply.github.com>
2024-04-11 17:05:18 +0200

    frontend>form & basic conversion

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  protected readonly Number = Number;
+  result?: number;
+  indexes?: number[] = [1, 0.93];
 
-  constructor() {}
+  constructor(private readonly fb: FormBuilder) {}
 
+  form = this.fb.group({
+    source: ['', Validators.required],
+    target: ['', Validators.required],
+    value: ['', Validators.required]
+  });
+
+  get source() {
+    return this.form.controls.source;
+  }
+
+  get target() {
+    return this.form.controls.target;
+  }
+
+  get value() {
+    return this.form.controls.value;
+  }
+
+  convert() {
+    var source = this.form.controls.source.value;
+    var target = this.form.controls.target.value;
+    var value = this.form.controls.value.value;
+
+    // @ts-ignore
+    this.result = value?.valueOf() / source?.valueOf() * target?.valueOf();
+
+    console.log(value + ' / ' + source + ' * ' + target + ' = ')
+    console.log(this.result)
+  }
 }
