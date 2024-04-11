be6acc3652c46f6c1dde14f3a191d139836b1aea
micdra01 <114472076+micdra01@users.noreply.github.com>
2024-04-11 22:30:24 +0200

    Update Utilities.cs

--- infrastructure/Utilities.cs
+++ infrastructure/Utilities.cs
@@ -1,17 +1,7 @@
 ﻿namespace infrastructure;
 public static class Utilities
 {
-    private static readonly Uri Uri = new Uri(Environment.GetEnvironmentVariable("pgconn")!);
-    
-    public static readonly string
-        ProperlyFormattedConnectionString = string.Format(
-            "Server={0};Database={1};User Id={2};Password={3};Port={4};Pooling=true;MaxPoolSize=3",
-            Uri.Host,
-            Uri.AbsolutePath.Trim('/'),
-            Uri.UserInfo.Split(':')[0],
-            Uri.UserInfo.Split(':')[1],
-            Uri.Port > 0 ? Uri.Port : 5432);
-    
+   
     public static readonly string
         MySqlConnectionString = "Server=4.231.252.47;Database=currencies;Uid=myuser;Pwd=mypassword;";
 }
