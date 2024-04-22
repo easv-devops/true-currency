809abf9b3c8e0ba6e9c631dbda8a27f7b63c8088
micdra01 <114472076+micdra01@users.noreply.github.com>
2024-04-22 15:38:56 +0200

    Env test

--- infrastructure/Utilities.cs
+++ infrastructure/Utilities.cs
@@ -3,5 +3,5 @@ public static class Utilities
 {
    
     public static readonly string
-        MySqlConnectionString = "Server=4.231.252.47;Database=currencies;Uid=myuser;Pwd=mypassword;";
+        MySqlConnectionString = Environment.GetEnvironmentVariable("currency_conn");
 }
