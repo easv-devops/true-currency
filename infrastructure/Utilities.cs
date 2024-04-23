97a955784cd4f522465f419afc011710c3b51c98
micdra01 <114472076+micdra01@users.noreply.github.com>
2024-04-23 14:40:18 +0200

    bad variable name

--- infrastructure/Utilities.cs
+++ infrastructure/Utilities.cs
@@ -3,5 +3,5 @@ public static class Utilities
 {
    
     public static readonly string
-        MySqlConnectionString = Environment.GetEnvironmentVariable("currency-conn");
+        MySqlConnectionString = Environment.GetEnvironmentVariable("currencyconn");
 }
